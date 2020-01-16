041ea6858b6ae1b5c05915ea1b2b0ba1d7c82cfc
tsubasa <gumirain@gmail.com>
2020-01-17 00:22:19 +0900

    test

--- learning/src/learning/Main.java
+++ learning/src/learning/Main.java
@@ -4,12 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-      String message = "Hello World";
+        String message = "Hello World";
 
-      for(int i = 0;i < 10; i++) {
+        for(int i = 0;i < 10; i++) {
     	  System.out.println(message + i);
     	  System.out.println(message + i);
+        }
 
-      }
+        int score = 90;
+        if(score > 90) {
+    	  System.out.println("OK!");
+        }else {
+          System.out.println("NG!");
+        }
 	}
 }
