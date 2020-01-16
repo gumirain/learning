ac206ac9f469cbf670629874902e077dfe0ddf2b
tsubasa <gumirain@gmail.com>
2020-01-16 23:57:22 +0900

    test

--- learning/src/learning/Main.java
+++ learning/src/learning/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-System.out.println("Hello World");
-	}
+      String message = "Hello World";
 
+      for(int i = 0;i < 10; i++) {
+    	  System.out.println(message + i);
+      }
+	}
 }
